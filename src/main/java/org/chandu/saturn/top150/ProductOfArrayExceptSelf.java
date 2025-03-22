4d381b2225538c0625a9decc9278605afffbdf84
Chandu Singamaneni <chandu.singamaneni@tutanota.com>
2025-03-21 23:06:36 -0400

    Product of Array Except Self

---
+++ src/main/java/org/chandu/saturn/top150/ProductOfArrayExceptSelf.java
@@ -0,0 +1,57 @@
+package org.chandu.saturn.top150;
+
+import java.util.Arrays;
+
+/**
+ * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+ * <p>
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ * <p>
+ * You must write an algorithm that runs in O(n) time and without using the division operation.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3,4]
+ * Output: [24,12,8,6]
+ * Example 2:
+ * <p>
+ * Input: nums = [-1,1,0,-3,3]
+ * Output: [0,0,9,0,0]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 2 <= nums.length <= 105
+ * -30 <= nums[i] <= 30
+ * The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
+ * <p>
+ * <p>
+ * Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+ **/
+
+public class ProductOfArrayExceptSelf {
+
+    public static void main(String[] args) {
+        int[] test1 = new int[]{1, 2, 3, 4};
+        ProductOfArrayExceptSelf pes = new ProductOfArrayExceptSelf();
+
+        Arrays.stream(pes.productExceptSelf(test1)).forEach(System.out::println);
+
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0, left = 1; i < n; ++i) {
+            ans[i] = left;
+            left *= nums[i];
+        }
+        for (int i = n - 1, right = 1; i >= 0; --i) {
+            ans[i] *= right;
+            right *= nums[i];
+        }
+        return ans;
+    }
+}
